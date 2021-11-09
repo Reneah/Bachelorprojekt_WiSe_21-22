4c183e7ffac0e70bcdc8651bcdbfd45b62fde983
Christian Katz <ChristianKatz@web.de>
2021-11-09 10:05:08 +0100

    [CHANGE] Besieged_1-3
    First Level
    - added UI to the noisy item
    - Set Player to the beginning of the level
    Second Level
    - added UI to the noisy item
    Third Level
    - added Fade Shader to the pillars
    - pause menu is functional now
    - change the wrong noisy item UI
    - set up enemies
    - new Routes hierarchy order
    - added general point visualization script
    - activated the looking Route visualization
    - changed the wrong scene at the restart
    - adapted the player speed to the other scenes
    - set Player to the beginning of the scene

--- Assets/02_Scripts/02_Enemies/LookAgentVisualization.cs
+++ Assets/02_Scripts/02_Enemies/LookAgentVisualization.cs
@@ -9,14 +9,14 @@ public class LookAgentVisualization : MonoBehaviour
     [Tooltip(" the color of the visualization")]
     [SerializeField] private Color32 _color;
     [Tooltip("the size of the agent visualization")]
-    [SerializeField] private float radius;
+    [SerializeField] private float _radius;
 
     [SerializeField] private GameObject _enemyLookPosition;
     
     private void OnDrawGizmos()
     {
         Gizmos.color = _color;
-        Gizmos.DrawSphere(transform.position, radius);
+        Gizmos.DrawSphere(transform.position, _radius);
         Gizmos.DrawLine(transform.position, _enemyLookPosition.transform.position);
     }
 }
