e929c1bb1b3e6ba5c8875d8374f2d4e99cb671b1
Christian Katz <ChristianKatz@web.de>
2021-12-11 15:15:24 +0100

    [CHANGE] Enemy Controller & Loot State
    - Enemy Loot Rotation is set by the LootSpot Transform
    - Fixed a Bug, that the enemy is jittering while looting
    - Fixed a Bug, that the enemy is still in the loot animation when he notices the player while looting
    - Player Flee Sound Radius was 0 and set it back to 6
    - Enemy will get the footsteps sound in Trigger Stay, not in Trigger Enter anymore, so that the sound location will be updated all the time
    - Fixed Code Error at the CollectProvisions.cs. Currently, no vision Marker is used, so I commented it out

--- Assets/02_Scripts/02_Enemies/EnemyLootState.cs
+++ Assets/02_Scripts/02_Enemies/EnemyLootState.cs
@@ -9,11 +9,15 @@ namespace Enemy.States
         {
             if (enemy.CanSeePlayer)
             {
+                enemy.Agent.isStopped = false;
+                enemy.AnimationHandler.LootSpot(false);
                 return EnemyController.EnemyVisionChaseState;
             }
 
             if (enemy.SoundNoticed)
             {
+                enemy.Agent.isStopped = false;
+                enemy.AnimationHandler.LootSpot(false);
                 return EnemyController.EnemySoundInvestigationState;
             }
 
@@ -25,7 +29,7 @@ namespace Enemy.States
                 enemy.Agent.isStopped = true;
                 
                 // rotates the enemy towards the loot spot
-                Quaternion _desiredDirection = Quaternion.Slerp(enemy.transform.rotation, Quaternion.LookRotation(enemy.LootSpotTransform.position - enemy.transform.position), enemy.SmoothRotation * Time.deltaTime);
+                Quaternion _desiredDirection = Quaternion.Slerp(enemy.transform.rotation, enemy.LootSpotTransform.rotation, enemy.SmoothRotation * Time.deltaTime);
                 enemy.transform.rotation = _desiredDirection;
             }
             
