ee4c1f8c2e07cc607af2dd51998daab6344ef909
Reneah <FranziskaSchuema>
2021-12-29 03:35:06 +0100

    ADD Step Vizualization Modes for Sneak SneakRun and Flee

--- Assets/02_Scripts/01_Player/StepVisualization.cs
+++ Assets/02_Scripts/01_Player/StepVisualization.cs
@@ -2,54 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using untitledProject;
 
 namespace BP
 {
     public class StepVisualization : MonoBehaviour
     {
-        [SerializeField] 
-        private Color myColor;
-        [SerializeField]
-        private float minSize;
-        [SerializeField]
-        private float maxSize;
-
-        [SerializeField]
-        private float scalingDuration;
-        [SerializeField]
-        private float fadeDuration;
+        private StepVisualizationManager myStepVisualizationManager;
         
         private ProjectorForLWRP.ProjectorForLWRP myProjector;
         
         private float currentSize;
 
         private Material myMaterial;
+        
+        private float minSize;
+
+        private float maxSize;
+
+        private PlayerController player;
+        private bool visualize;
+
+        private float scalingSpeed;
+        private float fadeDuration;
 
         // Start is called before the first frame update
         void Start()
         {
+            player = FindObjectOfType<PlayerController>();
+            myStepVisualizationManager = FindObjectOfType<StepVisualizationManager>();
             myProjector = GetComponent<ProjectorForLWRP.ProjectorForLWRP>();
+            scalingSpeed = 
             currentSize = myProjector.projector.orthographicSize;
             myMaterial = myProjector.projector.material;
-            myMaterial.color = myColor;
+            myMaterial.color = myStepVisualizationManager.Color;
+            minSize = myStepVisualizationManager.MinSize;
         }
 
         // Update is called once per frame
         void Update()
         {
-            myProjector.projector.orthographicSize = currentSize;
-            if (Input.GetKeyDown(KeyCode.L))
+            bool sprint = Input.GetKey(KeyCode.LeftShift);
+            maxSize = sprint ? myStepVisualizationManager.SneakRunSize : myStepVisualizationManager.SneakSize;
+            scalingSpeed = sprint ? myStepVisualizationManager.ScalingSpeedSneakRun : myStepVisualizationManager.ScalingSpeedSneak;
+            fadeDuration = sprint ? myStepVisualizationManager.FadeDurationSneakRun : myStepVisualizationManager.FadeDurationSneak;
+        
+            if (player.PlayerAnimationHandler.PlayerAnimator.GetBool("Flee"))
             {
-                VisualizeStep();
+                maxSize = myStepVisualizationManager.FleeSize;
+                scalingSpeed = myStepVisualizationManager.ScalingSpeedFlee;
+                fadeDuration = myStepVisualizationManager.FadeDurationFlee;
+            }
+            myProjector.projector.orthographicSize = currentSize;
+
+            if (visualize)
+            {
+                if (currentSize <= maxSize)
+                {
+                    scalingSpeed = maxSize + minSize + scalingSpeed;
+                    currentSize += scalingSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    FadeVisualization();
+                }
             }
         }
 
         public void VisualizeStep()
         {
             myMaterial.DOKill();
-            myMaterial.color = myColor;
-            currentSize = minSize;
-            DOTween.To(() => currentSize, x => currentSize = x, maxSize, scalingDuration).OnComplete(FadeVisualization);
+            myMaterial.color = myStepVisualizationManager.Color;
+            visualize = true;
+            //currentSize = minSize;
+
+            //DOTween.To(() => currentSize, x => currentSize = x, maxSize, myStepVisualizationManager.ScalingDuration).OnComplete(FadeVisualization);
         }
 
         public void FadeVisualization()
@@ -59,6 +86,8 @@ namespace BP
 
         public void ResetStepVisualization()
         {
+            visualize = false;
+            myMaterial.color = Color.black;
             currentSize = minSize;
         }
     }
